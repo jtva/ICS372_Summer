7a7ef150bb1010743aa0d5ad394489e8bf638014
damadi37 <dacia.cavalari@gmail.com>
2016-06-02 21:21:14 -0500

    Added Main Class

---
+++ src/java/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * The purpose of this project is to test to Git Repository with Eclipse
+ * @author Dacia Cavalari
+ * Jun 2, 2016
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Steve, June, and Armeya!");
+	}
+
+}
